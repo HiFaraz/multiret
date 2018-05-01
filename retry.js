6ce259c44e83a81865d3f56935291d3f74beda3d
Faraz Syed <hello@farazsyed.com>
2018-05-01 01:17:34 -0400

    fix: add default values for retry

--- retry.js
+++ retry.js
@@ -4,16 +4,22 @@ const pDelay = require('./delay');
  * Retry an async action, with exponential delay growth
  *
  * @param {AsyncFunction} fn
- * @param {number} interval
- * @param {number} factor
- * @param {boolean} forever
- * @param {number} limit
+ * @param {number} [interval=1000]
+ * @param {number} [factor=2]
+ * @param {boolean} [forever=false]
+ * @param {number} [limit=10]
  * @param {Function} [couldRetry=()=>true]
  * @returns {Promise<any[]>}
  */
 function retry(
   fn,
-  { interval, factor, forever, limit, couldRetry = () => true },
+  {
+    interval = 1000,
+    factor = 2,
+    forever = false,
+    limit = 10,
+    couldRetry = () => true,
+  },
 ) {
   async function attempt(remaining, delay) {
     const [error, result] = await fn();
